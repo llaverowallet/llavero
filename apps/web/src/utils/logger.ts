488d66342073074065e5866ed34f32eb49486464
Mariano Julio Vicario <elranu@gmail.com>
2023-10-07 09:29:07 -0300

    update to aws-sdk v3

--- apps/web/src/utils/logger.ts
+++ apps/web/src/utils/logger.ts
@@ -1,13 +1,11 @@
-import { CloudWatchLogs } from "aws-sdk";
-import { get } from "http";
-
+import { CloudWatchLogsClient, CreateLogStreamCommand, PutLogEventsCommand } from "@aws-sdk/client-cloudwatch-logs";
 
 class CloudWatchLogger {
     private static instance: CloudWatchLogger;
-    private cloudwatchlogs: CloudWatchLogs;
+    private cloudwatchlogs: CloudWatchLogsClient;
     private readonly LOG_GROUP_NAME;
     private constructor() {
-        this.cloudwatchlogs = new CloudWatchLogs({ region: "us-east-1" });
+        this.cloudwatchlogs = new CloudWatchLogsClient({ region: "us-east-1" });
         this.LOG_GROUP_NAME = process.env.LOG_GROUP_NAME ?? "noGroupName-error-check-env-variables";
     }
 
@@ -18,7 +16,7 @@ class CloudWatchLogger {
         return CloudWatchLogger.instance;
     }
 
-    public log(message: string) {
+    public async log(message: string) {
         const logStreamName = this.getLogStreamName();
         const logEvent = {
             message: message,
@@ -29,10 +27,10 @@ class CloudWatchLogger {
             logStreamName: logStreamName,
             logEvents: [logEvent],
         };
-        this.putLog(params);
+        await this.putLog(params);
     }
 
-    public error(message: string) {
+    public async error(message: string) {
         const logStreamName = this.getLogStreamName();
         const logEvent = {
             message: "ERROR:" + message,
@@ -43,18 +41,37 @@ class CloudWatchLogger {
             logStreamName: logStreamName,
             logEvents: [logEvent],
         };
-        this.putLog(params);
+        await this.putLog(params);
     }
 
-    /*private putLog(params: { logGroupName: string; logStreamName: string; logEvents: { message: string; timestamp: number; }[]; }) {
-        this.cloudwatchlogs.putLogEvents(params, function (err, data) {
-            if (err) {
-                console.log("Error logging to CloudWatch:", err);
-            } else {
-                console.log("Logged to CloudWatch:", data);
-            }
+    private async putLog(params: { logGroupName: string; logStreamName: string; logEvents: { message: string; timestamp: number; }[]; }) {
+        const logStreamName = params.logStreamName;
+        const logGroupName = params.logGroupName;
+        const logEvents = params.logEvents;
+        const paramsCreateStream = new CreateLogStreamCommand({
+            logGroupName: logGroupName,
+            logStreamName: logStreamName
         });
-    }*/
+        try {
+            await this.cloudwatchlogs.send(paramsCreateStream);
+        } catch (err: any) {
+            if (err.name !== 'ResourceAlreadyExistsException') {
+                console.log("Error creating log stream:", err);
+                return;
+            }
+        }
+        const paramsPutLog = new PutLogEventsCommand({
+            logGroupName: logGroupName,
+            logStreamName: logStreamName,
+            logEvents: logEvents
+        });
+        try {
+            const data = await this.cloudwatchlogs.send(paramsPutLog);
+            console.log("Logged to CloudWatch:", data);
+        } catch (err) {
+            console.log("Error logging to CloudWatch:", err);
+        }
+    }
 
     private getLogStreamName(): string {
         const now = new Date();
@@ -63,37 +80,6 @@ class CloudWatchLogger {
         const roundedTimeString = roundedMinutes.toString().padStart(2, '0');
         const logStreamName = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}-${now.getHours().toString().padStart(2, '0')}-${roundedTimeString}`;
         return logStreamName;
-    }
-
-    private putLog(params: { logGroupName: string; logStreamName: string; logEvents: { message: string; timestamp: number; }[]; }) {
-        const self = this;
-        const logStreamName = params.logStreamName;
-        const logGroupName = params.logGroupName;
-        const logEvents = params.logEvents;
-        const paramsCreateStream = {
-            logGroupName: logGroupName,
-            logStreamName: logStreamName
-        };
-        this.cloudwatchlogs.createLogStream(paramsCreateStream, function (err, data) {
-            if (err) {
-                if (err.code !== 'ResourceAlreadyExistsException') {
-                    console.log("Error creating log stream:", err);
-                    return;
-                }
-            }
-            const paramsPutLog = {
-                logGroupName: logGroupName,
-                logStreamName: logStreamName,
-                logEvents: logEvents
-            };
-            self.cloudwatchlogs.putLogEvents(paramsPutLog, function (err, data) {
-                if (err) {
-                    console.log("Error logging to CloudWatch:", err);
-                } else {
-                    console.log("Logged to CloudWatch:", data);
-                }
-            });
-        });
     }
 }
 
