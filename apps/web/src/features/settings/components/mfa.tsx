eebaf20cccc1e2201b01cee6f977706f831c456d
Ricardo Olarte <raop155@hotmail.com>
2023-12-28 10:51:11 -0500

    fix: MFA ui ux

--- apps/web/src/features/settings/components/mfa.tsx
+++ apps/web/src/features/settings/components/mfa.tsx
@@ -2,94 +2,115 @@ import { useEffect, useState } from 'react';
 import { Input } from '@/shared/components/ui/input';
 import { useSession } from 'next-auth/react';
 import QRCode from 'react-qr-code';
-import { associateSoftwareToken, setEnableMFA, verifySoftwareToken } from '@/shared/utils/mfa-actions';
-
-
+import {
+  associateSoftwareToken,
+  setEnableMFA,
+  verifySoftwareToken,
+} from '@/shared/utils/mfa-actions';
+import { Button } from '@/shared/components/ui/button';
+import { Copy } from 'lucide-react';
+import { CopyToClipboard } from '@/shared/components/ui/copy-to-clipboard';
+import { Badge } from '@/shared/components/ui/badge';
+import { Skeleton } from '@/shared/components/ui/skeleton';
 
 const MFA = () => {
-    const [qrCode, setQRCode] = useState('');
-    const [secretCode, setSecretCode] = useState('');
-    const [verificationCode, setVerificationCode] = useState('');
-    const [isVerified, setIsVerified] = useState(false);
-    const [error, setError] = useState('');
-    const { data } = useSession();
-    const [qrUri, setQrUri] = useState('');
-    
-    let session: string | undefined = undefined;
-    
-    const getTotpCodeURL = (issuer: string, username: string, secret: string): string =>
-        encodeURI(
-            `otpauth://totp/${issuer}:${username}?secret=${secret}&issuer=${issuer}`
-        );
+  const [secretCode, setSecretCode] = useState('');
+  const [verificationCode, setVerificationCode] = useState('');
+  const [isVerified, setIsVerified] = useState(false);
+  const [error, setError] = useState('');
+  const { data } = useSession();
+  const [qrUri, setQrUri] = useState('');
 
-    useEffect(() => {
-        async function checkMFA() { 
-            const token = (data as any).accessToken;
-            const isAssociated = await associateSoftwareToken(token);
-            if (isAssociated.SecretCode) {
-                setQrUri(getTotpCodeURL('Llavero', data?.user?.email || "", isAssociated.SecretCode || ""));//;
-                setSecretCode(isAssociated.SecretCode);
-                // eslint-disable-next-line react-hooks/exhaustive-deps
-                session = isAssociated.Session;
-            }
-        }
-        checkMFA();
-    }, [data, data?.user?.email]);
+  let session: string | undefined = undefined;
 
-    const handleVerifyMFA = async () => {
-        const token = (data as any).accessToken;
-        if (verificationCode.length === 6 && await verifySoftwareToken(verificationCode, token, session)) {
-            await setEnableMFA(token);
-            setError('');
-            setIsVerified(true);
-        } else {
-            setError('Invalid verification code');
-        }
-    };
+  const getTotpCodeURL = (issuer: string, username: string, secret: string): string =>
+    encodeURI(`otpauth://totp/${issuer}:${username}?secret=${secret}&issuer=${issuer}`);
 
-    return (
-        <div className="flex flex-col items-center">
-            {!isVerified ? (
-                <>
-                    <div className="mt-6">
-                        {secretCode && (
-                            <><QRCode
-                                size={256}
-                                className='w-full'
-                                value={qrUri || ''}
-                                viewBox={`0 0 256 256`} />
-                                <div>
-                                    <p>Secret Code: {secretCode}</p>
-                                </div></>
-                        )}
+  useEffect(() => {
+    async function checkMFA() {
+      const token = (data as any).accessToken;
+      const isAssociated = await associateSoftwareToken(token);
+      if (isAssociated.SecretCode) {
+        setQrUri(getTotpCodeURL('Llavero', data?.user?.email || '', isAssociated.SecretCode || '')); //;
+        setSecretCode(isAssociated.SecretCode);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        session = isAssociated.Session;
+      }
+    }
+    checkMFA();
+  }, [data, data?.user?.email]);
 
-                        <Input
-                            type="text"
-                            placeholder="Enter verification code"
-                            value={verificationCode}
-                            onChange={(e) => setVerificationCode(e.target.value)}
-                            className="border border-gray-300 rounded px-4 py-2"
-                        />
-                        <button
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
-                            onClick={handleVerifyMFA}
-                        >
-                            Verify MFA
-                        </button>
-                    </div>
-                    {error && (
-                        <div className="mt-4">
-                            <p className="text-red-500">{error}</p>
-                        </div>
-                    )}
-                </>
-            ) : (
-                <div className="bg-green-200 p-4 rounded">
-                    <p>Verification successful!</p>
+  const handleVerifyMFA = async () => {
+    const token = (data as any).accessToken;
+    if (
+      verificationCode.length === 6 &&
+      (await verifySoftwareToken(verificationCode, token, session))
+    ) {
+      await setEnableMFA(token);
+      setError('');
+      setIsVerified(true);
+    } else {
+      setError('Invalid verification code');
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {!isVerified ? (
+        <>
+          <div className="mt-6 flex flex-col justify-center gap-2">
+            {secretCode ? (
+              <>
+                <QRCode size={256} className="w-full" value={qrUri || ''} viewBox={`0 0 256 256`} />
+                <div className="flex gap-4 justify-center items-center mt-4">
+                  <span>Secret Code: </span>
+                  <CopyToClipboard textToCopy={secretCode || ''}>
+                    <Badge variant="outline" className="flex gap-2 py-2 cursor-pointer">
+                      {secretCode || ''} <Copy className="w-4 h-4" />
+                    </Badge>
+                  </CopyToClipboard>
                 </div>
+              </>
+            ) : (
+              <QRSkeleton />
             )}
+
+            <Input
+              type="text"
+              placeholder="Enter verification code"
+              value={verificationCode}
+              onChange={(e) => setVerificationCode(e.target.value)}
+              className="border border-gray-300 rounded px-4 py-2"
+            />
+
+            <div className="mx-auto">
+              <Button disabled={!secretCode} onClick={handleVerifyMFA}>
+                Verify MFA
+              </Button>
+            </div>
+          </div>
+          {error && (
+            <div className="mt-4">
+              <p className="text-red-500">{error}</p>
+            </div>
+          )}
+        </>
+      ) : (
+        <div className="bg-green-200 p-4 rounded">
+          <p>Verification successful!</p>
         </div>
-    );
+      )}
+    </div>
+  );
+};
+
+const QRSkeleton = () => {
+  return (
+    <div className="grid gap-2">
+      <Skeleton className="h-[256px] w-[256px]" />
+      <Skeleton className="h-[25px] w-full" />
+    </div>
+  );
 };
 
 export default MFA;
